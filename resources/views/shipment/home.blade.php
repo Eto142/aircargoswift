bf76f80f63273dfad28124aeb27913d240d4f490
eto <eto.saviour80@gmail.com>
2026-01-13 21:49:35 +0000

    first commit

--- resources/views/shipment/home.blade.php
+++ resources/views/shipment/home.blade.php
@@ -1499,3 +1499,16 @@
     </script>
 </body>
 </html>
+
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '8f008d65101f52381e9eda3d4d7d8d27b3cdcd31';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
